6e34a8143b5b91400e1fc125a7a6e84103447b78
Albert Cerfeda <cerfeda.albert@crfda.com>
2024-03-12 23:25:05 +0100

    Added code for task 2

---
+++ Homework2/task2.cpp
@@ -0,0 +1,152 @@
+#include "common.h"
+#include "mat.h"
+
+#define C (1.0 / 3.0)
+#define M_PI3 M_PI/3
+#define ALPHA 110.0 / 41.0
+
+using namespace std;
+
+void b2(mat* x, mat* y, mat* z) {
+    double t[12];
+
+    double recipCosines[6];
+    recipCosines[0] = 1;
+    recipCosines[1] = 1 / cos(M_PI3);
+    recipCosines[2] = 1 / cos(M_PI3 * 2);
+    recipCosines[3] = -1;
+    recipCosines[4] = recipCosines[2];
+    recipCosines[5] = recipCosines[1];
+
+    double outerSqrts[6];
+    outerSqrts[0] = 1/sqrt(x->data[0]);
+    outerSqrts[1] = 1/sqrt(x->data[1]);
+    outerSqrts[2] = 1/sqrt(x->data[2]);
+    outerSqrts[3] = 1/sqrt(x->data[3]);
+    outerSqrts[4] = 1/sqrt(x->data[4]);
+    outerSqrts[5] = 1/sqrt(x->data[5]);
+    
+    double outerCosine[3];
+    outerCosine[0] = cos(x->data[0]);
+    outerCosine[1] = cos(x->data[1]);
+    outerCosine[2] = cos(x->data[2]);
+    
+    // 2x loop unrolling
+    for (int i = 0; i < 100; i += 2) {
+        double sqrt1 = 1/sqrt(y->data[i * 100]);
+
+        double recipInnerSqrts[6];
+        recipInnerSqrts[0] = sqrt1 * outerSqrts[0];
+        recipInnerSqrts[1] = sqrt1 * outerSqrts[1];
+        recipInnerSqrts[2] = sqrt1 * outerSqrts[2];
+        recipInnerSqrts[3] = sqrt1 * outerSqrts[3];
+        recipInnerSqrts[4] = sqrt1 * outerSqrts[4];
+        recipInnerSqrts[5] = sqrt1 * outerSqrts[5];
+      
+        int j = 0;
+        // 6x loop unrolling
+        for (; j+6 < 100; j+=6) {
+            int t1_idx = i * 100 + j;
+            int t2_idx = (i + 1) * 100 + j;
+
+            t[0] = z->data[t1_idx] * recipInnerSqrts[0] + y->data[t1_idx];
+            t[1] = z->data[t2_idx] * recipCosines[0] + x->data[t2_idx];
+            t[2] = z->data[t1_idx+1] * recipInnerSqrts[1] + y->data[t1_idx+1];
+            t[3] = z->data[t2_idx+1] * recipCosines[1] + x->data[t2_idx+1];
+            t[4] = z->data[t1_idx+2] * recipInnerSqrts[2] + y->data[t1_idx+2];
+            t[5] = z->data[t2_idx+2] * recipCosines[2] + x->data[t2_idx+2];
+            t[6] = z->data[t1_idx+3] * recipInnerSqrts[3] + y->data[t1_idx+3];
+            t[7] = z->data[t2_idx+3] * recipCosines[3] + x->data[t2_idx+3];
+            t[8] = z->data[t1_idx+4] * recipInnerSqrts[4] + y->data[t1_idx+4];
+            t[9] = z->data[t2_idx+4] * recipCosines[4] + x->data[t2_idx+4];
+            t[10] = z->data[t1_idx+5] * recipInnerSqrts[5] + y->data[t1_idx+5];
+            t[11] = z->data[t2_idx+5] * recipCosines[5] + x->data[t2_idx+5];
+
+            z->data[t1_idx] = t[0] * outerCosine[0];
+            z->data[t2_idx] = t[1] * outerCosine[0];
+            z->data[t1_idx+1] = t[2] * outerCosine[1];
+            z->data[t2_idx+1] = t[3] * outerCosine[1];
+            z->data[t1_idx+2] = t[4] * outerCosine[2];
+            z->data[t2_idx+2] = t[5] * outerCosine[2];
+            z->data[t1_idx+3] = t[6] * outerCosine[0];
+            z->data[t2_idx+3] = t[7] * outerCosine[0];
+            z->data[t1_idx+4] = t[8] * outerCosine[1];
+            z->data[t2_idx+4] = t[9] * outerCosine[1];
+            z->data[t1_idx+5] = t[10] * outerCosine[2];
+            z->data[t2_idx+5] = t[11] * outerCosine[2];
+        }
+        
+        // Finish off last 4 elements
+        for (; j < 100; j+=4) {
+            int t1_idx = i * 100 + j;
+            int t2_idx = (i + 1) * 100 + j;
+
+            t[0] = z->data[t1_idx] * recipInnerSqrts[0] + y->data[t1_idx];
+            t[1] = z->data[t2_idx] * recipCosines[0] + x->data[t2_idx];
+            t[2] = z->data[t1_idx+1] * recipInnerSqrts[1] + y->data[t1_idx+1];
+            t[3] = z->data[t2_idx+1] * recipCosines[1] + x->data[t2_idx+1];
+            t[4] = z->data[t1_idx+2] * recipInnerSqrts[2] + y->data[t1_idx+2];
+            t[5] = z->data[t2_idx+2] * recipCosines[2] + x->data[t2_idx+2];
+            t[6] = z->data[t1_idx+3] * recipInnerSqrts[3] + y->data[t1_idx+3];
+            t[7] = z->data[t2_idx+3] * recipCosines[3] + x->data[t2_idx+3];
+            
+
+            z->data[t1_idx] = t[0] * outerCosine[0];
+            z->data[t2_idx] = t[1] * outerCosine[0];
+            z->data[t1_idx+1] = t[2] * outerCosine[1];
+            z->data[t2_idx+1] = t[3] * outerCosine[1];
+            z->data[t1_idx+2] = t[4] * outerCosine[2];
+            z->data[t2_idx+2] = t[5] * outerCosine[2];
+            z->data[t1_idx+3] = t[6] * outerCosine[0];
+            z->data[t2_idx+3] = t[7] * outerCosine[0];
+        }
+    }
+    for (int j = 0; j < 10000; j+=4) {
+            double t0 = z->data[j];
+            double t1 = z->data[j+1];
+            double t2 = z->data[j+2];
+            double t3 = z->data[j+3];
+            
+            t0 = t0 > 0 ? t0*ALPHA+1 : abs(t0)+1;
+            t1 = t1 > 0 ? t1*ALPHA+1 : abs(t1)+1;
+            t2 = t2 > 0 ? t2*ALPHA+1 : abs(t2)+1;
+            t3 = t3 > 0 ? t3*ALPHA+1 : abs(t3)+1;
+
+            z->data[j] = t0;
+            z->data[j+1] = t1;
+            z->data[j+2] = t2;
+            z->data[j+3] = t3;
+    }
+}
+
+void slow_performance1(mat* x, mat* y, mat* z) {
+    double t1;
+    for (int i = 0; i < z->n1; i++) {
+        for (int j = 0; j < z->n2; j++) {
+            if (i % 2) {
+                t1 = mat_get(z, i, j) / cos(C * M_PI * j) + mat_get(x, i, j);
+            } else {
+                t1 = mat_get(z, i, j) /
+                         (sqrt(mat_get(y, i, 0)) * sqrt(mat_get(x, 0, j % 6))) +
+                     mat_get(y, i, j);
+            }
+            mat_set(z, i, j, t1 * cos(mat_get(x, 0, j % 3)));
+        }
+    }
+    for (int i = 0; i < z->n1; i++) {
+        for (int j = 0; j < z->n2; j++) {
+            mat_activate(z, i, j, ALPHA);
+        }
+    }
+}
+
+void maxperformance(mat* x, mat* y, mat* z) { b2(x, y, z); }
+
+/*
+ * Called by the driver to register your functions
+ * Use add_function(func, description) to add your own functions
+ */
+void register_functions() {
+    add_function(&slow_performance1, "slow_performance1", 1);
+    add_function(&maxperformance, "maxperformance", 1);
+}
