5de00e0b7f86e65e156a2ca53096b9d7e3c13750
Albert Cerfeda <cerfeda.albert@crfda.com>
2024-04-12 20:15:25 +0200

    Refined cache simulator

--- Homework4/ex1a.c
+++ Homework4/ex1a.c
@@ -4,10 +4,7 @@
 #include <string.h>
 #include <math.h>
 
-// #define BLOCK_SIZE 32
-// #define CACHE_SIZE 1024
-// #define ASSOCIAVITY 1
-// #define SETS (CACHE_SIZE / BLOCK_SIZE / ASSOCIAVITY)
+
 
 char* mem_offset;
 
@@ -22,6 +19,7 @@ struct cache_block {
 }; 
 struct cache_set {
     struct cache_block* blocks;
+    int* LRU;
 };
 struct cache_s {
     struct cache_set* sets;
@@ -36,7 +34,7 @@ struct cache_s {
 };
 typedef struct cache_s cache_t;
 
-cache_t* init_cache(int BLOCK_SIZE, int CACHE_SIZE, int ASSOCIAVITY) {
+cache_t* cache_init(int BLOCK_SIZE, int CACHE_SIZE, int ASSOCIAVITY) {
     cache_t* cache = malloc(sizeof(cache_t));
     cache->BLOCK_SIZE = BLOCK_SIZE;
     cache->CACHE_SIZE = CACHE_SIZE;
@@ -48,10 +46,12 @@ cache_t* init_cache(int BLOCK_SIZE, int CACHE_SIZE, int ASSOCIAVITY) {
         for (int j = 0; j < ASSOCIAVITY; j++) {
             cache->sets[i].blocks[j].content_addr = -1;
         }
+        cache->sets[i].LRU = calloc(ASSOCIAVITY, sizeof(int));
+        cache->sets[i].LRU[0] = 1;
     }
     return cache;
 }
-void free_cache(cache_t* cache) {
+void cache_free(cache_t* cache) {
     for (int i = 0; i < cache->SETS; i++) {
         free(cache->sets[i].blocks);
     }
@@ -59,18 +59,54 @@ void free_cache(cache_t* cache) {
     free(cache);
 }
 
-int access_cache(cache_t *cache, char* ptr) {
-    ptrdiff_t mem_addr = ptr - mem_offset;
-    int set = (int)floor(mem_addr / cache->BLOCK_SIZE) % cache->SETS;
-    ptrdiff_t block_addr = mem_addr - (mem_addr % cache->BLOCK_SIZE);
-    cache->total_accesses++;
-    if (cache->sets[set].blocks[0].content_addr == block_addr) {
-        cache->total_hits++;
-        return 1;
+void cache_update_LRU(cache_t* cache, int set_idx, int block_idx) {
+    struct cache_set set = cache->sets[set_idx];
+
+    set.LRU[block_idx] = 0;
+    for (int i = 0; i < cache->ASSOCIAVITY; i++) {
+        if (i != block_idx) {
+            set.LRU[i]++;
+        }
     }
+}
+int cache_get_LRU(cache_t* cache, int set_idx) {
+    struct cache_set set = cache->sets[set_idx];
+    struct cache_block *block;
+    int max_LRU = 0;
+    int max_LRU_idx = 0;
+    for (int i = 0; i < cache->ASSOCIAVITY; i++) {
+        if (set.LRU[i] > max_LRU) {
+            max_LRU = set.LRU[i];
+            max_LRU_idx = i;
+        }
+    }
+    return max_LRU_idx;
+}
+
+int cache_access(cache_t *cache, char* ptr) {
+    cache->total_accesses++;
+
+    ptrdiff_t mem_addr = ptr - mem_offset;
+    int set_idx = (int)floor(mem_addr / cache->BLOCK_SIZE) % cache->SETS;
+    ptrdiff_t block_addr = mem_addr - (mem_addr % cache->BLOCK_SIZE);
+    struct cache_set set = cache->sets[set_idx];
+    
+    struct cache_block block;
+
+    for (int i = 0; i < cache->ASSOCIAVITY; i++) {
+        if (set.blocks[i].content_addr == block_addr) {
+            // Hit
+            cache->total_hits++;
+            cache_update_LRU(cache, set_idx, i);
+            return 1;
+        }
+    }
+
     // Miss
     cache->total_misses++;
-    cache->sets[set].blocks[0].content_addr = block_addr;
+    int LRU_idx = cache_get_LRU(cache, set_idx);
+    set.blocks[LRU_idx].content_addr = block_addr;
+    cache_update_LRU(cache, set_idx, LRU_idx);
     return 0;
 }
 
@@ -87,11 +123,11 @@ cache_t* default_cache;
 
 
 double stencil(double* A, int lda) {
-    access_cache(default_cache, A + 0);
+    cache_access(default_cache, A + 0);
     double acc = A[0];
-    access_cache(default_cache, A - lda);
+    cache_access(default_cache, A - lda);
     acc += A[-lda];
-    access_cache(default_cache, A + lda);
+    cache_access(default_cache, A + lda);
     acc += A[lda];
     return acc;
 }
@@ -99,26 +135,47 @@ double stencil(double* A, int lda) {
 void comp(double* A, double* O, int n, int s) {
     for (int i = 1; i < n - 1; i++) {
         for (int j = 0, jj = 0; j < n; j += s, jj++) {
-            access_cache(default_cache, O + (i - 1) * n / s + jj); 
             O[(i - 1) * n / s + jj] = stencil(&A[i * n + j], n);
+            cache_access(default_cache, O + (i - 1) * n / s + jj); 
         }
     }
 }
 
+void ex1a(int associativity) {
+    int s = 1, n = 8;
+    printf("\nn: %d s: %d\n", n, s);
 
-int main(int argc, char const *argv[])
-{   
-    const int n = strtol(argv[1], NULL, 10);
-    const int s = strtol(argv[2], NULL, 10);
-    printf("n: %d\ns: %d\n", n, s);
-
-    default_cache = init_cache(32, 1024, 1);
+    default_cache = cache_init(32, 1024, associativity);
 
     double* mem = malloc(n*n*2*sizeof(double));
     mem_offset = (char*)mem;
     
     comp(mem, mem+n*n, n, s);
     
-    printf("Total accesses: %ld\nTotal misses: %ld\nTotal hits: %ld\nMiss ratio: %f\n", default_cache->total_accesses, default_cache->total_misses, default_cache->total_hits, (double)default_cache->total_misses/default_cache->total_accesses);
-    free_cache(default_cache);
+    printf("Accesses: %ld\nMisses: %ld\nHits: %ld\nMiss rate: %f\%\n", default_cache->total_accesses, default_cache->total_misses, default_cache->total_hits, (double)default_cache->total_misses/default_cache->total_accesses*100);
+    cache_free(default_cache);
+}
+void ex1b(int associativity) {
+    int s = 2, n = 16;
+    printf("\nn: %d s: %d\n", n, s);
+
+    default_cache = cache_init(32, 1024, associativity);
+
+    double* mem = malloc(n*n*2*sizeof(double));
+    mem_offset = (char*)mem;
+    
+    comp(mem, mem+n*n, n, s);
+    
+    printf("Accesses: %ld\nMisses: %ld\nHits: %ld\nMiss rate: %f\%\n", default_cache->total_accesses, default_cache->total_misses, default_cache->total_hits, (double)default_cache->total_misses/default_cache->total_accesses*100);
+    cache_free(default_cache);
+}
+
+
+
+int main(int argc, char const *argv[]) {
+    printf("======\nAssociativity 1\n");   
+    ex1a(1);
+    ex1b(1);
+    printf("======\nAssociativity 2\n");
+    ex1b(2);
 }
